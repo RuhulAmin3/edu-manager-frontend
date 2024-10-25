0956f68731f6c48fcadcea7bd3f2301a23215395
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2024-10-26 00:26:15 +0600

    setup ant design initially

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/index.tsx";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#00b96b",
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: "#f6ffed",
+        },
+      }}
+    />
     <RouterProvider router={routes} />
   </StrictMode>
 );
