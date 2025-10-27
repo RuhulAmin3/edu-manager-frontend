d64211bbf1e4fab8fe1f9dd057fb0683a3091758
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2025-10-27 15:03:11 +0600

    fix: fix server url issues

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -23,7 +23,7 @@ import {
 import { tagTypes } from "./tag-types";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://edu-manager-backend.onrender.com/api/v1/",
+  baseUrl: "https://edu-manager-server.up.railway.app/api/v1/",
   // baseUrl: "http://localhost:4000/api/v1/",
   prepareHeaders: (headers) => {
     const token = getFromLocalStorage(ACCESS_TOKEN_KEY);
